cc12786dfdf8501e837e0d6b5f8df40470b21eb7
Pankaj50000 <japankaj282@gmail.com>
2023-10-17 11:00:25 +0530

    submission

---
+++ sslab2/hol19/ss219.c
@@ -0,0 +1,31 @@
+// Question: Create a FIFO file by `mkfifo` library function and `mknod` system call
+
+#include <sys/types.h> 
+#include <sys/stat.h>  
+#include <fcntl.h>     
+#include <unistd.h>    
+#include <stdio.h>     
+
+void main()
+{
+    char *mkfifoName = "./mymkfifo";    
+    char *mknodName = "./mymknod-fifo"; 
+
+    int mkfifo_status, mknod_status; 
+
+    // Using `mkfifo` library function
+    mkfifo_status = mkfifo(mkfifoName, S_IRWXU);
+
+    if (mkfifo_status == -1)
+        perror("Error while creating FIFO file!");
+    else
+        printf("Succesfully created FIFO file. Check using `ll` or `ls -l` command!\n");
+
+    // Using `mknod` system call
+    mkfifo_status = mknod(mknodName, __S_IFIFO | S_IRWXU, 0);
+
+    if (mknod_status == -1)
+        perror("Error while creating FIFO file!");
+    else
+        printf("Succesfully created FIFO file. Check using `ll` or `ls -l` command!\n");
+}
